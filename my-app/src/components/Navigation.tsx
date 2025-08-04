65cf3e7992ccff76dfd5b0d1b9f1c27145893502
Esther <esther@boardroomco.ca>
2025-08-04 13:35:04 -0700

    chore: changed favicon and tab title

--- my-app/src/components/Navigation.tsx
+++ my-app/src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import TitleLogo from '../assets/titlelogo.svg';
+import TitleLogo from '../assets/favicon.png';
 
 const Navigation: React.FC = () => {
   const { user, isAuthenticated, logout } = useAuth();
